0e77d3d24abb9b0be18e5a070544a4e7272472e7
victorhugb <victorhugob@hypergroup.com.br>
2024-05-17 17:17:11 -0300

    :recycle: refatorado a estrutura da pagina de signin

--- front/src/pages/Signin/index.tsx
+++ front/src/pages/Signin/index.tsx
@@ -1,5 +1,12 @@
 import { useForm, FormProvider } from "react-hook-form";
-import { Box, Typography, Link, Grid } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Link,
+  Container,
+  Checkbox,
+  FormControlLabel,
+} from "@mui/material";
 import ApiBack from "../../services/base-back.js";
 import ControlledText from "../../shared/Fields/TextField.js";
 import { useToast } from "../../context/ToastContext.js";
@@ -42,114 +49,97 @@ function SignIn() {
   }
 
   return (
-    <>
-      <Grid
-        container
-        spacing={2}
-        margin={0}
-        justifyContent="center"
-        alignItems="center"
-        padding={0}
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <Box
         sx={{
-          width: "100vw",
-          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "50%",
+          border: "1px solid #ccc",
+          padding: "20px",
+          borderRadius: "10px",
+          boxShadow: "0 0 10px #ccc",
         }}
       >
-        <Grid
-          item
-          xs={12}
-          md={8}
-          sx={{
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundImage: "url(/image3.png)",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "left",
-          }}
+        <Typography
+          component="h1"
+          variant="h2"
+          fontWeight={600}
+          color={"#FFC03D"}
         >
-          <Typography variant="h4" gutterBottom fontWeight={600}>
-            Bem-vindo!
-          </Typography>
-          <Typography variant="h6" gutterBottom fontWeight={600}>
-            Faça seu login para começar!
-          </Typography>
-          <Typography fontWeight={600}>
-            Descubra novas funcionalidades e acompanhe seu o rastreamento do seu
-            veiculo em <b>Tempo Real</b>!
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          md={4}
-          sx={{
-            width: "100vw",
-            height: "100%",
-            border: "1px solid #ccc",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            padding: "20px",
-          }}
-        >
-          <Typography component="h1" variant="h2" fontWeight={400}>
-            Login
-          </Typography>
-          <FormProvider {...methods}>
-            <Box
-              component="form"
-              onSubmit={handleSubmit(handleLogin)}
-              noValidate
+          Login
+        </Typography>
+        <FormProvider {...methods}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit(handleLogin)}
+            noValidate
+            sx={{
+              mt: 8,
+              width: "100%",
+              gap: "20px",
+              display: "flex",
+              flexDirection: "column",
+              alignContent: "center",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            gap={4}
+          >
+            <ControlledText
+              name="email"
+              control={control}
+              defaultValue=""
+              label="E-mail"
+              fullWidth
+              size="medium"
+            />
+            <ControlledText
+              name="password"
+              control={control}
+              defaultValue=""
+              label="Senha"
+              fullWidth
+              type="password"
+              size="medium"
+            />
+            <FormControlLabel control={<Checkbox />} label="Lembrar-me" />
+            <LoadingButton
+              type="submit"
+              variant="contained"
               sx={{
-                mt: 8,
-                width: "100%",
-                gap: "20px",
-                display: "flex",
-                flexDirection: "column",
-                alignContent: "center",
-                justifyContent: "center",
-                alignItems: "center",
+                mb: 2,
+                backgroundColor: "#FFC03D",
+                color: "black",
               }}
-              gap={4}
+              fullWidth
             >
-              <ControlledText
-                name="email"
-                control={control}
-                defaultValue=""
-                label="E-mail"
-                fullWidth
-                size="medium"
-              />
-              <ControlledText
-                name="password"
-                control={control}
-                defaultValue=""
-                label="Senha"
-                fullWidth
-                type="password"
-                size="medium"
-              />
-              <LoadingButton
-                type="submit"
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                fullWidth
-              >
-                Acessar
-              </LoadingButton>
-              <Link href="/signup" variant="body2">
-                {"Não tem conta? Cadastre-se"}
-              </Link>
-            </Box>
-          </FormProvider>
-        </Grid>
-      </Grid>
-    </>
+              Acessar
+            </LoadingButton>
+            <Link
+              href="/signup"
+              variant="body2"
+              sx={{
+                color: "black",
+              }}
+            >
+              {"Não tem conta? Cadastre-se"}
+            </Link>
+          </Box>
+        </FormProvider>
+      </Box>
+    </Container>
   );
 }
 
