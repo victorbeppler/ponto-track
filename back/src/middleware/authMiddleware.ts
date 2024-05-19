fc035839c95ead4cd467f52dd81ae97241d01fe4
victorhugb <victorhugob@hypergroup.com.br>
2024-05-19 17:11:13 -0300

    :construction:  Middleware da Autenticação
    Foi necessário a criação para adicionar como um validador do token de acesso.

---
+++ back/src/middleware/authMiddleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET!, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user as any;
+    next();
+  });
+};
